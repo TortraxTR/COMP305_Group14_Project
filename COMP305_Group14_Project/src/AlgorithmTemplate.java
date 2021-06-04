00ba233cf1aa85a438f8184672d12c305295b496
ya-v-uz <56271907+ya-v-uz@users.noreply.github.com>
2021-06-04 23:25:27 +0300

    greedy_algorithm added

--- COMP305_Group14_Project/src/AlgorithmTemplate.java
+++ COMP305_Group14_Project/src/AlgorithmTemplate.java
@@ -1,5 +1,5 @@
-import java.util.Scanner; 
-import java.util.HashMap; 
+import java.util.*;
+
 
 public class MainAlgorithm {
 	public static int given_value_count; 
@@ -17,6 +17,8 @@ public class MainAlgorithm {
 		System.out.print("enter num of optimization ints\n");
 		asked_value_count = read.nextInt(); // num of k
 		
+		
+		
 		for(int i = 0; i< given_value_count; i++) { 
 			System.out.print("give pixel val\n");
 			int value = read.nextInt(); 
@@ -26,31 +28,58 @@ public class MainAlgorithm {
 		} 
 		
 		
+		System.out.println(original_pixels);
+		MainAlgorithm NewAlgo = new MainAlgorithm();	
+		int val[] = {83};
+		System.out.printf( "Calculated Loss: %d\n" ,(NewAlgo.CalculateLoss(original_pixels, val)));
+		
+		
+	
+		//int optimization_val = read.nextInt(); // will be discarded later, because algorithm will internally assign it.
+		
+		
+		int[] opt_vals = NewAlgo.GenerateGreedyOpt(original_pixels, asked_value_count);
+		System.out.printf( "Calculated Loss with Greedy Algorithm: %d\n" ,(NewAlgo.CalculateLoss(original_pixels, opt_vals)));
 		
 		
 		
-		MainAlgorithm NewAlgo = new MainAlgorithm();
-		System.out.println("Enter a value to be tested as a singular k");
-		int optimization_val = read.nextInt(); // will be discarded later, because algorithm will internally assign it.
-		System.out.printf("Loss is: %d\n",NewAlgo.CalculateLoss(original_pixels, optimization_val));
 		read.close(); 
 		} 
 	
 	
+	/*  
+ 	LOSS = px_count.(px_val - assigned_opt)^2 
+ 	
+  * * * * 	
+ 	* * 
+ 	 * 
+ */
 
 
 
-public int CalculateLoss(HashMap<Integer,Integer> hash, int val_tobe_tested) {	 // val_tobe_tested will be array for now let's get only 1 
+
 	
-		int total = 0;
+public int CalculateLoss(HashMap<Integer,Integer> hash, int[] k_val_array) {	 
 	
-		for(int i : original_pixels.keySet()) {
-			 System.out.printf("%d number of pixel in value: %d\n", original_pixels.get(i), i);
-			 int sq = (val_tobe_tested - i) * (val_tobe_tested - i); // we will get the corresponding k value here  according to our algorithm function
-			 total += (sq * original_pixels.get(i));
+int size = k_val_array.length;	
+int total_loss = 0;
+int val = 0;
+int min = 2147483647; // largest possible integer
+
+
+for(int i : hash.keySet()) {
+
+		for(int x=0; x < size; x++) {
+			val = hash.get(i) * (i - k_val_array[x]) * (i - k_val_array[x]);
+			if( val < min ) {
+				min = val;
+			}	
 		}
-	
-		return total;
+	System.out.printf("for hash key %d, minimum is %d\n", i, min);	
+	total_loss += min; 	
+	min = 2147483647;
+}
+return total_loss;
 }
 
 
@@ -60,6 +89,30 @@ public int GenerateOptK(HashMap<Integer,Integer> hash, int allowed_k_num) {
 }
 
 
+public int[] GenerateGreedyOpt (HashMap<Integer,Integer> hash, int allowed_k_num) { // returns greedy k values for opt.
+	
+	List<Integer> px_counts = new ArrayList<Integer>();
+	
+	for(int i : hash.keySet()) {
+			px_counts.add(hash.get(i));
+		}
+	
+	Collections.sort(px_counts, Collections.reverseOrder());
+	int[] k_vals = new int[allowed_k_num];
+	
+	for(int i=0; i<allowed_k_num; i++) {
+		k_vals[i] = px_counts.get(i); 
+	}
+	
+	
+	return k_vals;
+
+
+} 
+
+
 }
+
+
 
 
