2cf52f4ed9b0760a0d0111da1376792f2ca0da2b
ya-v-uz <56271907+ya-v-uz@users.noreply.github.com>
2021-06-05 11:07:05 +0300

    Input format has changed

--- COMP305_Group14_Project/src/AlgorithmTemplate.java
+++ COMP305_Group14_Project/src/AlgorithmTemplate.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.io.*;
+
 
 
 public class MainAlgorithm {
@@ -6,81 +8,93 @@ public class MainAlgorithm {
 	public static int asked_value_count; 
 	int your_val;
 	public static HashMap<Integer, Integer> original_pixels = new HashMap<Integer, Integer>(); 
-	public static HashMap<Integer, Integer> new_pixels = new HashMap<Integer, Integer>(); 
+	public static HashMap<Double, Integer> new_pixels = new HashMap<Double, Integer>(); 
 	
-	public static void main(String[] args) { // todo auto-generated method stub 
-		
-		Scanner read = new Scanner(System.in); 
-		
-		System.out.print("enter distinct pixel value\n");
-		given_value_count = read.nextInt();  // num of distinct pixel value
-		System.out.print("enter num of optimization ints\n");
-		asked_value_count = read.nextInt(); // num of k
+	public static void main(String[] args) throws IOException { // todo auto-generated method stub 
 		
 		
+		 FileReader fr =
+			      new FileReader("./text.txt");
 		
-		for(int i = 0; i< given_value_count; i++) { 
-			System.out.print("give pixel val\n");
-			int value = read.nextInt(); 
-			System.out.print("give pixel count\n");
-			int count = read.nextInt(); 
-			original_pixels.put(value,count); 
-		} 
+		 Scanner sc = new Scanner(fr);
+		 
+		 sc.useDelimiter("\\p{javaWhitespace}+");
+		 
+		 given_value_count = Integer.parseInt(sc.next()); 
+		 asked_value_count = Integer.parseInt(sc.next()); 
 		
+		
+		 
+		  
+		   while (sc.hasNext()) {
+			 int a = Integer.parseInt(sc.next()); 
+			 int b = Integer.parseInt(sc.next()); 
+			 original_pixels.put(a, b);
+		     
+		    } 
+		    sc.close();
+		    
+    
 		
 		System.out.println(original_pixels);
 		MainAlgorithm NewAlgo = new MainAlgorithm();	
-		int val[] = {83};
-		System.out.printf( "Calculated Loss: %d\n" ,(NewAlgo.CalculateLoss(original_pixels, val)));
+		/* int val[] = {83};
+		System.out.printf( "Calculated Loss: %d\n" ,(NewAlgo.CalculateLoss(original_pixels, val))); */
 		
 		int[] opt_vals = NewAlgo.GenerateGreedyOpt(original_pixels, asked_value_count);
 		
 		for(int i=0; i<opt_vals.length; i++) {
 			System.out.printf("opt value %d: %d\n", i, opt_vals[i]);
 		}
-		System.out.printf( "Calculated Loss with Greedy Algorithm: %d\n" ,(NewAlgo.CalculateLoss(original_pixels, opt_vals)));
 		
+		 System.out.printf( "Calculated Loss with Greedy Algorithm: %d\n" ,(NewAlgo.CalculateLoss(original_pixels, opt_vals))); 
 		
-		
-		read.close(); 
 		} 
 	
 	
 	/*  
+ 	
  	LOSS = px_count.(px_val - assigned_opt)^2 
  	
   * * * * 	
  	* * 
  	 * 
+ 
  */
 
 	
-public int CalculateLoss(HashMap<Integer,Integer> hash, int[] k_val_array) {	 
+public long CalculateLoss(HashMap<Integer,Integer> hash, int[] k_val_array) {	 
 	
 int size = k_val_array.length;	
-int total_loss = 0;
-int val = 0;
-int min = 2147483647; // largest possible integer
+long total_loss = 0;
+long val = 0;
+long min = Long.MAX_VALUE; // largest possible integer
 
 
 for(int i : hash.keySet()) {
-
+		
+		
+	
 		for(int x=0; x < size; x++) {
-			val = hash.get(i) * (i - k_val_array[x]) * (i - k_val_array[x]);
+			val = ((i - k_val_array[x]) * (i - k_val_array[x])) * (long) hash.get(i) ;
+			// System.out.printf("for hash key %d and opt_val = %d , current val is %d and current hash is: %d\n", i, k_val_array[x],val, hash.get(i));
 			if( val < min ) {
-				min = val;
-			}	
+				min = val;	
+			}
+			
+			
 		}
-	System.out.printf("for hash key %d, minimum is %d\n", i, min);	
-	total_loss += min; 	
+		
+	
+	total_loss = total_loss +  min; 	
 	min = 2147483647;
 }
 return total_loss;
 }
 
 
-public int GenerateOptK(HashMap<Integer,Integer> hash, int allowed_k_num) {
-	// main method where we find optimal approx. for hash keys, return type may change to another hashmap. 
+public int GenerateDFT(HashMap<Integer,Integer> hash, int allowed_k_num) {
+	// DFT algorithm to choose (x - k)^2 roots accordingly to find min
 	return 0;
 }
 
@@ -104,14 +118,10 @@ public int[] GenerateGreedyOpt (HashMap<Integer,Integer> hash, int allowed_k_num
 	
 	}
 	}
-	
-	
-	
-	
 	return k_vals;
-
-
 } 
+
+
 
 public int[] GenerateAlternateOpt(HashMap<Integer,Integer> old, HashMap<Integer, Integer> updated, int allowed_k_num) {
 	
@@ -136,7 +146,4 @@ public int[] GenerateAlternateOpt(HashMap<Integer,Integer> old, HashMap<Integer,
 	return k_vals;
 }
 
-
 }
-
-
